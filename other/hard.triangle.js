e817ed832ae0f9ee8d04c417348b12d021fcd7f9
Taufiqurrahman <rahman.yusuftaufiq@gmail.com>
2020-06-18 21:01:46 +0700

    Simple Refactor

--- other/hard.triangle.js
+++ other/hard.triangle.js
@@ -1,11 +1,11 @@
 /* --------------------------------------------------------------- TRIANGLE */
 const triangle = p => {
-    const arr = Array(p).fill().map(v => Array(p + p - 1).fill(' '))
+    const arr = [...Array(p)].map(() => Array(p + p - 1).fill(' '))
     arr.forEach((v, k) => {
         if (k === p - 1)
             arr[k] = v.map((sv, sk) => sk % 2 === 0 ? '*' : ' ')
         else
-            [v[(p - 1) - k], v[(p - 1) + k]] = ['*', '*']
+            [v[p - 1 - k], v[p - 1 + k]] = ['*', '*']
     })
     return '\n' + arr.map(v => v.join``).join('\n') + '\n'
 }
