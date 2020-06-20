2888944eadbc298e26476269a024cbb903541a29
Taufiqurrahman <rahman.yusuftaufiq@gmail.com>
2020-06-20 18:34:55 +0700

    Refactor

--- other/expert.spiral-square.js
+++ other/expert.spiral-square.js
@@ -1,37 +1,42 @@
-/* --------------------------------------------------------------- SPIRAL SQUARE */
-const makeSpiralSquare = p => {
-    const arr = Array(p).fill().map(() => Array(p).fill(' '))
-    for (i = 0, j = 0; i <= Math.floor(p / 4); i += 1, j+= 2) {
-        if (i !== Math.floor(p / 4) || p % 4 >= 1) {
-            for (const [k] of arr[j].entries()) {
-                if (k > (j - 1) && k < (p - (j - 1))) arr[j][k] = '*'
-            }
-        }
-        if (i !== Math.floor(p / 4) || p % 4 >= 2 || p % 4 === 0) {
-            for (const [k] of arr.entries()) {
-                if (k > (j - 1) && k < (p - j)) arr[k][j] = '*'
-            }
-        }
-        if (i !== Math.floor(p / 4) || p % 4 >= 3 || p % 4 === 0) {
-            for (const [k] of arr[p - (j + 1)].entries()) {
-                if (k > (j - 1) && k < (p - j)) arr[p - (j + 1)][k] = '*'
-            }
-        }
-        if (i !== Math.floor(p / 4) || p % 4 === 0) {
-            for (const [k] of arr.entries()) {
-                if (k > (j + 1) && k < (p - j)) arr[k][p - (j + 1)] = '*'
-            }
-        }
-    }
-    return '\n' + arr.map(v => v.join(' ')).join('\n') + '\n'
-}
+// --------------------------------------------------------------- SPIRAL SQUARE
+const makeSpiralSquare = (p) => {
+  const arr = [...Array(p)].map(() => Array(p).fill(' '));
 
-/* Check Spiral Square --------------------------------------------------------------- */
-makeSpiralSquare(7) // Expected Result
-//    * * * * * * *
-//    *            
-//    *   * * * * *
-//    *   *       *
-//    *   * * *   *
-//    *           *
-//    * * * * * * *
+  for (let i = 0, j = 0; i <= Math.floor(p / 4); i += 1, j += 2) {
+    if (i !== Math.floor(p / 4) || p % 4 >= 1) {
+      arr[j].forEach((v, k) => {
+        if (k > (j - 1) && k < (p - (j - 1))) arr[j][k] = '*';
+      });
+    }
+    if (i !== Math.floor(p / 4) || p % 4 >= 2 || p % 4 === 0) {
+      arr.forEach((v, k) => {
+        if (k > (j - 1) && k < (p - j)) arr[k][j] = '*';
+      });
+    }
+    if (i !== Math.floor(p / 4) || p % 4 >= 3 || p % 4 === 0) {
+      arr[p - (j + 1)].forEach((v, k) => {
+        if (k > (j - 1) && k < (p - j)) arr[p - (j + 1)][k] = '*';
+      });
+    }
+    if (i !== Math.floor(p / 4) || p % 4 === 0) {
+      arr.forEach((v, k) => {
+        if (k > (j + 1) && k < (p - j)) arr[k][p - (j + 1)] = '*';
+      });
+    }
+  }
+
+  return `\n${arr.map((v) => v.join(' ')).join('\n')}\n`;
+};
+
+// Check Spiral Square ---------------------------------------------------------------
+makeSpiralSquare(7);
+/**
+ * Expected Result
+ * * * * * * * *
+ * *
+ * *   * * * * *
+ * *   *       *
+ * *   * * *   *
+ * *           *
+ * * * * * * * *
+ */
--- other/hard.triangle.js
+++ other/hard.triangle.js
@@ -1,19 +1,25 @@
-/* --------------------------------------------------------------- TRIANGLE */
-const triangle = p => {
-    const arr = [...Array(p)].map(() => Array(p + p - 1).fill(' '))
-    arr.forEach((v, k) => {
-        if (k === p - 1)
-            arr[k] = v.map((sv, sk) => sk % 2 === 0 ? '*' : ' ')
-        else
-            [v[p - 1 - k], v[p - 1 + k]] = ['*', '*']
-    })
-    return '\n' + arr.map(v => v.join``).join('\n') + '\n'
-}
+// --------------------------------------------------------------- TRIANGLE
+const triangle = (p) => {
+  const arr = [...Array(p)].map(() => Array(p + p - 1).fill(' '));
 
-/* Check Triangle --------------------------------------------------------------- */
-triangle(5) // Expected Result
-//        *    
-//       * *   
-//      *   *  
-//     *     * 
-//    * * * * *
+  arr.forEach((v, k) => {
+    if (k === p - 1) {
+      arr[k] = v.map((sv, sk) => (sk % 2 === 0 ? '*' : ' '));
+    } else {
+      [arr[k][(p - 1) - k], arr[k][(p - 1) + k]] = ['*', '*'];
+    }
+  });
+
+  return `\n${arr.map((v) => v.join``).join('\n')}\n`;
+};
+
+// Check Triangle ---------------------------------------------------------------
+triangle(5);
+/**
+ * Expected Result
+ *     *
+ *    * *
+ *   *   *
+ *  *     *
+ * * * * * *
+ */
